a4009f234fc535ae0757a52333650a0115a7d2ac
Roberto0611 <ochoacuevasr@gmail.com>
2023-11-15 08:20:40 -0700

    Finish The Draw function

--- JS/gameplay.js
+++ JS/gameplay.js
@@ -4,17 +4,27 @@
 let cells = boardSize * boardSize;
 let cell = [];
 
-const cell1 = document.getElementById('cell-0-0');
+//Set turns and get turns text to display it
+turns = ['x','o'];
+actualTurn = turns[0]; 
+turnstext = document.getElementById('turnText');
+//Set the text to show the actual turn
+turnstext.textContent = "it's" + " "+ actualTurn + " " +"turn" ;
 
-turns = ['x','o']; 
-
+//Function to show te X's and the O's
 function draw(index){
-    index.addEventListener('click',function(){
-        index.textContent = turns[0];
-        console.log(index)
-        console.log('separacion')
+    index.addEventListener('click',function(){ //Add event listener to all the cells
+        index.textContent = actualTurn; //Draw the X or O
+
+        if(actualTurn == turns[0]){// Change turn logic
+            actualTurn = turns[1];
+            turnstext.textContent = "it's" + " "+ actualTurn + " " +"turn" ; //display next turn
+        }else{
+            actualTurn = turns[0];
+            turnstext.textContent = "it's" + " "+ actualTurn + " " +"turn" ; //display next turn
+        }
     })
 }
-arrayCells.forEach(function(index) {
-    draw(index);
+arrayCells.forEach(function(index) { //For each to add the event listener to all the cells
+    draw(index);//Run the function
 }); 
